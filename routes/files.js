b33b41a7df3d2afb4ba0015777a5ad5200c5d8d1
tuhinpal <me@thetuhin.com>
2022-09-19 10:45:31 +0530

    Added rename feature

--- routes/files.js
+++ routes/files.js
@@ -25,9 +25,13 @@ router.get("/", (req, res) => {
         space: true,
       }),
       createdAt: stat.birthtime.toLocaleString(),
+      createdAtObj: stat.birthtime,
       id: Buffer.from(x).toString("hex"),
     };
   });
+
+  // sort files by date
+  files = files.sort((a, b) => b.createdAtObj - a.createdAtObj);
 
   res.render("files", {
     files: files,
@@ -48,6 +52,28 @@ router.get("/delete/:id", (req, res) => {
   try {
     let id = Buffer.from(req.params.id, "hex").toString("utf-8");
     fs.unlinkSync(__dirname + "/../public/" + id);
+  } catch (error) {}
+
+  res.redirect(req.query.redirect || "/files");
+});
+
+router.get("/rename/:id", (req, res) => {
+  try {
+    let id = Buffer.from(req.params.id, "hex").toString("utf-8");
+
+    let newname = req.query.name;
+
+    // if new name does not have extension, add the old one
+    try {
+      if (!newname.includes(".")) {
+        newname += id.substring(id.lastIndexOf("."));
+      }
+    } catch (_) {}
+
+    fs.renameSync(
+      __dirname + "/../public/" + id,
+      __dirname + "/../public/" + newname
+    );
   } catch (error) {}
 
   res.redirect(req.query.redirect || "/files");
